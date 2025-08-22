06250552b1fc61bb48dee54507b433fe45d46918
azhardzakwan789 <azhar.dzakwan.azizi-2020@stmm.unair.ac.id>
2025-08-22 22:17:41 +0700

    Adding greyscale for differencing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
-import numpy
+import numpy as np
+import matplotlib as plt
 
 video_input = input("Enter Video Path: ")
 cap = cv2.VideoCapture(video_input)
@@ -8,39 +9,42 @@ frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 fps = cap.get(cv2.CAP_PROP_FPS)
 print(f"Total Frame: {frame_count} , FPS: {fps}")
 
-frame_start = int(input(f"Pilih frame awal between 0 - {frame_count-1}: "))
-frame_end = int(input(f"Pilih frame akhir between {frame_start+1} - {frame_count}: "))
-
-
 ret, prev_frame = cap.read()
 if not ret:
     print("Couldnt read the first frame from video")
-    exit()    
+    exit()
 
-for i in range(int(frame_count)):
+frame_start = int(input(f"Pilih frame awal between 0 - {frame_count-1}: "))
+frame_end = int(input(f"Pilih frame akhir between {frame_start+1} - {frame_count}: "))    
+frame_list = []
+
+for frames in range(int(frame_count)):
     ret, curr_frame = cap.read()
 
-    if i <= frame_start:
+    if frames <= frame_start:
         continue
 
-    if not ret or i >= frame_end:
+    if not ret or frames >= frame_end:
         break
-
     
-    
-    frame_diff = cv2.absdiff(prev_frame, curr_frame)
-    cv2.imshow("Current Frame", curr_frame)
-    cv2.imshow("Frame Difference", frame_diff)
+    prev_grey = cv2.cvtColor(prev_frame, cv2.COLOR_RGB2GRAY)
+    curr_grey = cv2.cvtColor(curr_frame, cv2.COLOR_RGB2GRAY)
 
-    cv2.imwrite(f"result{i}.png", frame_diff)
+    frame_diff = cv2.absdiff(prev_grey, curr_grey)
+
+    cv2.imwrite(f"rgb_result{frames}.png", frame_diff) 
+    
+    # frame_list.append(frame_diff)
 
     prev_frame = curr_frame
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(30) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+    
 
 
 
