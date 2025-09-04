ec85f5f56fc8e2bfc939ae3fcdc5d62731ffd068
Elite <adzkawan@gmail.com>
2025-09-04 21:22:24 +0700

    Revisi unnecessarry code outside objective

--- function.py
+++ function.py
@@ -6,33 +6,25 @@ def image_differencing (cap):
     fps = cap.get(cv2.CAP_PROP_FPS)
     print(f"Total Frame: {frame_count} , FPS: {fps}")
     
-    #Retrieve the first frame of a video
-    ret, prev_frame = cap.read()
-    if not ret:
-        print("Couldnt read the first frame from video")
-        exit()
-    
-    #Determine which frames to be processed
-    frame_start = int(input(f"Pilih frame awal between 0 - {frame_count-1}: "))
-    frame_end = int(input(f"Pilih frame akhir between {frame_start+1} - {frame_count}: "))    
-    
-    for frames in range(int(frame_count)):
+    frame_start = int(frame_count/2)
+    frame_end = frame_start + 5
+
+    for frames in range(frame_end - frame_start):
+        curr = frames + frame_start
+        cap.set(cv2.CAP_PROP_POS_FRAMES, curr)
         ret, curr_frame = cap.read()
 
-        if frames <= frame_start:
-            continue
-        
-        if not ret or frames > frame_end:
-            break
+        cap.set(cv2.CAP_PROP_POS_FRAMES, curr+1)
+        ret, next_frame = cap.read()
+
 
         #convert frames into grayscale for a better view of differences (reduce noises)
-        prev_grey = cv2.cvtColor(prev_frame, cv2.COLOR_RGB2GRAY)
         curr_grey = cv2.cvtColor(curr_frame, cv2.COLOR_RGB2GRAY)
+        next_grey = cv2.cvtColor(next_frame, cv2.COLOR_RGB2GRAY)
     
-        frame_diff = cv2.absdiff(prev_grey, curr_grey)
+        frame_diff = cv2.absdiff(curr_grey, next_grey)
        
-        cv2.imwrite(f"result {frames-1} and {frames}.png", frame_diff) 
-        prev_frame = curr_frame
+        cv2.imwrite(f"result {curr} and {curr+1}.png", frame_diff) 
         
     #closes video files to frees up memories
     cap.release()
